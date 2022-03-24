42a5868fcc21a913c3b6117951c5e28173cb8568
Vasanth <vasanth@admins-MacBook-Pro.local>
2022-03-24 15:59:31 +0530

    Changed String/Methods

--- src/String/Methods.java
+++ src/String/Methods.java
@@ -9,25 +9,6 @@ public class Methods {
         String s2="Vasanth";
         String s3="How r you?";
 
-        //subString method
-        System.out.println("Substring: "+s1.substring(0,12));
 
-        //contains method
-        System.out.println("Contains: "+s1.contains("Love"));
-
-        //concat method
-        System.out.println("Concat: "+s1.concat(" Muruganandam"));
-
-        //replace method
-        String s=s3.replace("r","are");
-        System.out.println("Replace: "+s);
-
-        //to upper case method
-        String str="java tutorial from banfico";
-        System.out.println("Uppercase : "+str.toUpperCase());
-
-        //to lower case method
-        String str1="JAVA TUTORIAL FROM BANFICO";
-        System.out.println("Lowercase : "+str.toLowerCase());
     }
 }
